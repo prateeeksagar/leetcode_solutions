dcd3e725992f6858c1568c8ddd3e3a6f0149af94
PRATEEK SAGAR <53829966+prateeeksagar@users.noreply.github.com>
2024-08-22 14:54:11 +0530

    Time: 18 ms (39.53%) | Memory: 16.9 MB (89.26%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        // i can solve this problem using bfs kahn algorithm (topo sort)
+
+
+        // creating adjaceny list
+        vector<int> adj[numCourses]; // O(V+E)
+        for(int i = 0; i < prerequisites.size(); i++) {
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            adj[v].push_back(u);
+        }
+
+        vector<int> indegrees(numCourses, 0); // O(numCourses)
+        for(int i = 0; i < numCourses; i++) {
+            for(auto &it: adj[i]) {
+                indegrees[it]++;
+            }
+        }
+
+        queue<int> q; //O(numCourses)
+        for(int i = 0 ; i < numCourses; i++) {
+            if(indegrees[i] == 0) q.push(i);
+        }
+
+        vector<int> ans; // O(N)
+        while(!q.empty()) { // O(V+E)
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for(auto &it: adj[node]) {
+                indegrees[it]--;
+                if(indegrees[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+
+        if(ans.size() == numCourses) return ans;
+        return {};
+
+
+    }
+};
