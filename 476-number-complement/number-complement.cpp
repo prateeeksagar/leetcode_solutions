e074fc518f7af77b6401ca66c1216ac73107bd6b
PRATEEK SAGAR <53829966+prateeeksagar@users.noreply.github.com>
2024-08-22 19:08:01 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (33.08%) - LeetSync

--- 476-number-complement/number-complement.cpp
+++ 476-number-complement/number-complement.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int findComplement(int num) {
-        vector<int> bits;
-
         int number = num;
-        while (number) {
-            int x = number % 2 ? 0 : 1;
-            bits.push_back(x);
-            number = number / 2;
-        }
 
+        int i = 0;
         int ans = 0;
-        for (int i = 0; i < bits.size(); i++) {
-            ans += bits[i] * pow(2, i);
+        while (number) {
+            ans += (number % 2 ? 0 : 1) * pow(2, i++);
+            number = number / 2;
         }
 
         return ans;
