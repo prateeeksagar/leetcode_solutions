8bc29847eb6a77f8bb76f7c3f2d26b4579e734b8
PRATEEK SAGAR <53829966+prateeeksagar@users.noreply.github.com>
2025-10-08 11:51:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (15.82%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -2,15 +2,22 @@ class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
         string mergedString = "";
-        int i = 0;
-        while(i < word1.length() || i < word2.length()) {
-            if(i < word1.length()) {
-                mergedString.push_back(word1[i]);
-            }
-            if(i < word2.length()) {
-                mergedString.push_back(word2[i]);
-            }
-            i++;
+        int n1 = word1.length();
+        int n2 = word2.length();
+
+        mergedString.reserve(n1 + n2);
+
+        int min_len = min(n1, n2);
+
+        for(int i = 0; i < min_len; i++) {
+            mergedString.push_back(word1[i]);
+            mergedString.push_back(word2[i]);
+        }
+
+        if(n1 > n2) {
+            mergedString.append(word1, min_len, n1-min_len);
+        } else if(n2 > n1) {
+            mergedString.append(word2, min_len, n2 - min_len);
         }
         return mergedString;
      }
